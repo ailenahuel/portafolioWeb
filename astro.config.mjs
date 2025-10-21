c234d091d73c77ea52bea6bd5e1bf9b527e37895
Ailen Nahuel <ailenahuelcanete@gmail.com>
2025-10-21 03:46:59 -0300

    se agrego cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://www.ailenahuel.cl/',
   base: '/',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: cloudflare()
 });
